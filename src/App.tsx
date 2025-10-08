378cda7485aeedbcf2ab1b8d321ab4d9ed5ec553
BhrameswararaoYadlapalli <76121967+BhrameswararaoYadlapalli@users.noreply.github.com>
2025-10-07 20:58:47 -0400

    counter changed

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1 className="number">{count}</h1>
     </div>
     <div className="buttons">
-        <button className="button" onClick={() =>setCount(count + 10)} > +1 </button>
+        <button className="button" onClick={() =>setCount(count + 1)} > ClickMe </button>
     </div>
     </>
  
